38aa442e2513419e2f51c61c86034febbc1cbe3c
crferb <crferb@gmail.com>
2018-03-13 18:54:58 -0700

    Added homework 2

---
+++ CSHP220 Assignment 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSHP220_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            //Problem 1
+            var helloFilter = users.Where(t => t.Password.ToLower().Contains("hello"));
+            foreach (var user in helloFilter)
+            Console.WriteLine(user.Name);
+
+            //Problem 2
+            users.RemoveAll(t => t.Password == t.Name.ToLower());
+
+            //Problem 3
+            users.Remove(users.First(t => t.Password.ToLower().Contains("hello")));
+            
+            foreach (var user in users)
+            Console.WriteLine(user.Name, user.Password);
+        }
+    }
+}
